87482d0e9fa0325060c52359b5853273a7e3968d
Kevin Dodds <kevin@unomena.com>
2022-04-27 15:46:23 +0200

    Update page footer styling format
    Signed-off-by: Kevin Dodds <kevin@unomena.com>

---
+++ pages/customers.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageProvider from 'src-new/components/PageProvider';
+
+type Props = {};
+
+const Customers = (props: Props) => {
+  return <PageProvider displayTitle="Customers">Customers</PageProvider>;
+};
+
+export default Customers;
---
+++ src-new/components/PageProvider.tsx
@@ -0,0 +1,73 @@
+/** @jsxRuntime classic /
+/* @jsx jsx */
+
+import React, { useEffect, useState } from 'react';
+
+import { jsx } from '@emotion/react';
+import { Box } from '@mui/material';
+import { styled } from '@mui/system';
+
+import PageHeader from 'src/components/PageHeader';
+import PageFooter from 'src-new/components/PageFooter';
+import PageHead from 'src/components/PageHead';
+
+const PageContainer = styled(Box)`
+  position: relative;
+  width: 100%;
+  min-height: 100%;
+`;
+
+const defaultTitle = 'Upbound - The Universal Cloud Platform';
+const defaultDescription =
+  'The Upbound universal cloud platform empowers you to manage infrastructure, eliminate configuration drift, and ' +
+  'empower developers with self-service infrastructure.';
+
+const PageProvider: React.FC<{
+  isHeaderVisible?: boolean;
+  isFooterVisible?: boolean;
+  isOverflowVisible?: boolean;
+  displayTitle?: string;
+  metaTitle?: string;
+  metaDescription?: string;
+}> = ({
+  children,
+  isHeaderVisible = true,
+  isFooterVisible = true,
+  isOverflowVisible: isOverflowVisibleProp = true,
+  displayTitle = defaultTitle,
+  metaTitle = displayTitle,
+  metaDescription = defaultDescription,
+}) => {
+  const [isOverflowVisible, setOverflowVisible] = useState(isOverflowVisibleProp);
+
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     window.scrollTo(0, 0);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    document.body.classList.toggle(
+      'overflow-hidden',
+      isOverflowVisible === false || isOverflowVisibleProp === false
+    );
+    return () => document.body.classList.remove('overflow-hidden');
+  }, [isOverflowVisible, isOverflowVisibleProp]);
+
+  return (
+    <PageContainer id="page-container">
+      <PageHead
+        displayTitle={displayTitle}
+        metaTitle={metaTitle}
+        metaDescription={metaDescription}
+      />
+      <PageHeader isHeaderVisible={isHeaderVisible} setOverflowVisible={setOverflowVisible} />
+      <Box sx={{ overflow: 'hidden' }}>
+        {children}
+        <PageFooter isFooterVisible={isFooterVisible} />
+      </Box>
+    </PageContainer>
+  );
+};
+
+export default PageProvider;
