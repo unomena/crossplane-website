85d4748a22a41630ec66f6ca7ca99b6e3b68c6fc
Kevin Dodds <kevin@unomena.com>
2022-05-05 11:19:47 +0200

    Fix sitemap url
    Signed-off-by: Kevin Dodds <kevin@unomena.com>

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://upbound.unomena.io/',
+  siteUrl: process.env.SITE_URL || 'https://www.upbound.io/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
