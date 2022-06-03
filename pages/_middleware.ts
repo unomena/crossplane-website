6d222e31bf852e4fdbff03a58fcd6c70f62f104a
Kevin Dodds <kevin@unomena.com>
2022-06-03 13:37:55 +0200

    Temp add up-67 to middleware no_auth_urls
    Signed-off-by: Kevin Dodds <kevin@unomena.com>

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -7,6 +7,7 @@ const no_auth_urls = [
   'www.dev-deba7a0e.u6d.dev',
   'staging-eikeagoo.upbound.services',
   'www.staging-eikeagoo.upbound.services',
+  'marketing-site-git-up-67-upboundio.vercel.app',
 ];
 
 export function middleware(req: NextRequest) {
