cab2ec62652e8d743544b66d2d12ab5b7b7897d8
Pete Lumbis <pete@upbound.io>
2022-12-01 12:01:35 -0500

    Update docs URLs and rewrites
    Signed-off-by: Pete Lumbis <pete@upbound.io>

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ export const sitemap = '/sitemap-0.xml';
 
 export const githubUrl = 'https://github.com/crossplane/crossplane';
 export const slackUrl = 'https://slack.crossplane.io/';
-export const docsUrl = 'https://crossplane.io/docs';
+export const docsUrl = 'https://docs.crossplane.io';
 export const blogUrl = 'https://blog.crossplane.io/';
 export const twitterUrl = 'https://twitter.com/crossplane_io';
 export const youtubeUrl = 'https://www.youtube.com/channel/UC19FgzMBMqBro361HbE46Fw';
