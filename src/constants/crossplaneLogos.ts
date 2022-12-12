b2e937ade0e079b4ef981fbacc54b91ce7f92168
Kevin Dodds <kevin@unomena.com>
2022-12-12 09:25:13 +0200

    Add more logos
    Signed-off-by: Kevin Dodds <kevin@unomena.com>
    Signed-off-by: Nicole <nicole@unomena.com>

--- src/constants/crossplaneLogos.ts
+++ src/constants/crossplaneLogos.ts
@@ -47,46 +47,44 @@ import sap from 'public/home-page/crossplane-logos/046-sap.svg';
 import stone from 'public/home-page/crossplane-logos/047-stone.svg';
 import tigera from 'public/home-page/crossplane-logos/048-tigera.svg';
 import vmware from 'public/home-page/crossplane-logos/049-vmware.svg';
-// import adidas from 'public/home-page/crossplane-logos/adidas-light.svg';
-// import americanExpress from 'public/home-page/crossplane-logos/american-express-light.svg';
-// import asapp from 'public/home-page/crossplane-logos/asapp-light.svg';
-// import beamary from 'public/home-page/crossplane-logos/beamary-light.svg';
-// import behance from 'public/home-page/crossplane-logos/behance-light.svg';
-// import bitly from 'public/home-page/crossplane-logos/bitly-light.svg';
-// import bmwGroup from 'public/home-page/crossplane-logos/bmw-group-light.svg';
-// import cgi from 'public/home-page/crossplane-logos/cgi-light.svg';
-// import civicActions from 'public/home-page/crossplane-logos/civic-actions-light.svg';
-// import cobli from 'public/home-page/crossplane-logos/cobli-light.svg';
-// import contentful from 'public/home-page/crossplane-logos/contentful-light.svg';
-// import cultureAmp from 'public/home-page/crossplane-logos/culture-amp-light.svg';
-// import cyberint from 'public/home-page/crossplane-logos/cyberint-light.svg';
-// import delair from 'public/home-page/crossplane-logos/delair-light.svg';
-// import emnify from 'public/home-page/crossplane-logos/emnify-light.svg';
-// import enervee from 'public/home-page/crossplane-logos/enervee-light.svg';
-// import evaneos from 'public/home-page/crossplane-logos/evaneos-light.svg';
-// import exxonmobil from 'public/home-page/crossplane-logos/exxonmobil-light.svg';
-// import gigster from 'public/home-page/crossplane-logos/gigster-light.svg';
-// import glassdoor from 'public/home-page/crossplane-logos/glassdoor-light.svg';
-// import glossier from 'public/home-page/crossplane-logos/glossier--light.svg';
-// import goodRx from 'public/home-page/crossplane-logos/good-rx-light.svg';
-// import helloFresh from 'public/home-page/crossplane-logos/hello-fresh-light.svg';
-// import iheartRadio from 'public/home-page/crossplane-logos/iheart-radio-light.svg';
-// import mandiant from 'public/home-page/crossplane-logos/mandiant-light.svg';
-// import maze from 'public/home-page/crossplane-logos/maze-light.svg';
-// import morpheus from 'public/home-page/crossplane-logos/morpheus-light.svg';
-// import nextel from 'public/home-page/crossplane-logos/nextel-light.svg';
-// import plivo from 'public/home-page/crossplane-logos/plivo-light.svg';
-// import realtor from 'public/home-page/crossplane-logos/realtor-light.svg';
-// import resDiary from 'public/home-page/crossplane-logos/res-diary-light.svg';
-// import sanity from 'public/home-page/crossplane-logos/sanity-light.svg';
-// import sensorTower from 'public/home-page/crossplane-logos/sensor-tower-light.svg';
-// import trivago from 'public/home-page/crossplane-logos/trivago-light.svg';
-// import ubiwhere from 'public/home-page/crossplane-logos/ubiwhere-light.svg';
-// import vccLive from 'public/home-page/crossplane-logos/vcc-live-light.svg';
-// import verizon from 'public/home-page/crossplane-logos/verizon-light.svg';
-// import visa from 'public/home-page/crossplane-logos/visa-light.svg';
-// import yunify from 'public/home-page/crossplane-logos/yunify-light.svg';
-// import sixsense from 'public/home-page/crossplane-logos/6sense-light.svg';
+import adidas from 'public/home-page/crossplane-logos/adidas-original.svg';
+import asapp from 'public/home-page/crossplane-logos/asapp-original.svg';
+import beamary from 'public/home-page/crossplane-logos/beamary-original.svg';
+import behance from 'public/home-page/crossplane-logos/behance-original.svg';
+import bitly from 'public/home-page/crossplane-logos/bitly-original.svg';
+import cgi from 'public/home-page/crossplane-logos/cgi-original.svg';
+import civicActions from 'public/home-page/crossplane-logos/civic-actions-original.svg';
+import cobli from 'public/home-page/crossplane-logos/cobli-original.svg';
+import contentful from 'public/home-page/crossplane-logos/contentful-original.svg';
+import cultureAmp from 'public/home-page/crossplane-logos/culture-amp-original.svg';
+import cyberint from 'public/home-page/crossplane-logos/cyberint-original.svg';
+import delair from 'public/home-page/crossplane-logos/delair-original.svg';
+import emnify from 'public/home-page/crossplane-logos/emnify-original.svg';
+import enervee from 'public/home-page/crossplane-logos/enervee-original.svg';
+import evaneos from 'public/home-page/crossplane-logos/evaneos-original.svg';
+import exxonmobil from 'public/home-page/crossplane-logos/exxonmobil-original.svg';
+import gigster from 'public/home-page/crossplane-logos/gigster-original.svg';
+import glassdoor from 'public/home-page/crossplane-logos/glassdoor-original.svg';
+import glossier from 'public/home-page/crossplane-logos/glossier-original.svg';
+import goodRx from 'public/home-page/crossplane-logos/good-rx-original.svg';
+import helloFresh from 'public/home-page/crossplane-logos/hello-fresh-original.svg';
+import iheartRadio from 'public/home-page/crossplane-logos/iheart-radio-original.svg';
+import mandiant from 'public/home-page/crossplane-logos/mandiant-original.svg';
+import maze from 'public/home-page/crossplane-logos/maze-original.svg';
+import morpheus from 'public/home-page/crossplane-logos/morpheus-original.svg';
+import nextel from 'public/home-page/crossplane-logos/nextel-original.svg';
+import plivo from 'public/home-page/crossplane-logos/plivo-original.svg';
+import realtor from 'public/home-page/crossplane-logos/realtor-original.svg';
+import resDiary from 'public/home-page/crossplane-logos/res-diary-original.svg';
+import sanity from 'public/home-page/crossplane-logos/sanity-original.svg';
+import sensorTower from 'public/home-page/crossplane-logos/sensor-tower-original.svg';
+import trivago from 'public/home-page/crossplane-logos/trivago-original.svg';
+import ubiwhere from 'public/home-page/crossplane-logos/ubiwhere-original.svg';
+import vccLive from 'public/home-page/crossplane-logos/vcc-live-original.svg';
+import verizon from 'public/home-page/crossplane-logos/verizon-original.svg';
+import visa from 'public/home-page/crossplane-logos/visa-original.svg';
+import yunify from 'public/home-page/crossplane-logos/yunify-original.svg';
+import sixsense from 'public/home-page/crossplane-logos/6sense-original.svg';
 
 const crossplaneLogos = [
   absa,
@@ -138,46 +136,44 @@ const crossplaneLogos = [
   stone,
   tigera,
   vmware,
-  // adidas,
-  // americanExpress,
-  // asapp,
-  // beamary,
-  // behance,
-  // bitly,
-  // bmwGroup,
-  // cgi,
-  // civicActions,
-  // cobli,
-  // contentful,
-  // cultureAmp,
-  // cyberint,
-  // delair,
-  // emnify,
-  // enervee,
-  // evaneos,
-  // exxonmobil,
-  // gigster,
-  // glassdoor,
-  // glossier,
-  // goodRx,
-  // helloFresh,
-  // iheartRadio,
-  // mandiant,
-  // maze,
-  // morpheus,
-  // nextel,
-  // plivo,
-  // realtor,
-  // resDiary,
-  // sanity,
-  // sensorTower,
-  // trivago,
-  // ubiwhere,
-  // vccLive,
-  // verizon,
-  // visa,
-  // yunify,
-  // sixsense,
+  adidas,
+  asapp,
+  beamary,
+  behance,
+  bitly,
+  cgi,
+  civicActions,
+  cobli,
+  contentful,
+  cultureAmp,
+  cyberint,
+  delair,
+  emnify,
+  enervee,
+  evaneos,
+  exxonmobil,
+  gigster,
+  glassdoor,
+  glossier,
+  goodRx,
+  helloFresh,
+  iheartRadio,
+  mandiant,
+  maze,
+  morpheus,
+  nextel,
+  plivo,
+  realtor,
+  resDiary,
+  sanity,
+  sensorTower,
+  trivago,
+  ubiwhere,
+  vccLive,
+  verizon,
+  visa,
+  yunify,
+  sixsense,
 ];
 
 export default crossplaneLogos;
